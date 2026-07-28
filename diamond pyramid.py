8cb17eaf5276ccf4705b6bba91f67d2f0be0a37b
Pratik Singh <simnilpratik@gmail.com>
2026-07-28 10:38:55 +0530

    Add files via upload

---
+++ diamond pyramid.py
@@ -0,0 +1,13 @@
+n=int(input("enter number of rows"))
+for i in range(0,n):
+    print('',end='')
+for j in range(0,2*i+1):
+    print("*", end="")
+print()
+
+for i in range(0,n):
+ for j in range(0,i):
+     print("  ", end="")
+ for j in range (0,2*(n-i)-1):
+            print("*", end="")
+print()
